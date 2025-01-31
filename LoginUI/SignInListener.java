fdab1dce36096242006a966693bff2b16b5cb78c
vessahnjoya <vessahnjoyatchuentelouisnathan@gmail.com>
2025-01-31 20:21:18 +0100

    new Login UI

---
+++ LoginUI/SignInListener.java
@@ -0,0 +1,34 @@
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+import javax.swing.JTextField;
+
+
+
+public class SignInListener implements ActionListener {
+
+    private JFrame frame;
+    private JTextField userNameField;
+    private JTextField passwordField;
+    public SignInListener(JTextField usernameTextField, JTextField passwordTextField,JFrame frame){
+        this.userNameField = usernameTextField;
+        this.passwordField = passwordTextField;
+        this.frame = frame;
+    }
+    @Override
+    public void actionPerformed(ActionEvent e) {
+       
+        SignInVerification signIn = new SignInVerification();
+        if(signIn.login(userNameField.getText(), passwordField.getText())){
+            JOptionPane.showMessageDialog(frame, "LogIn Succesfull");
+        }else{
+            JOptionPane.showMessageDialog(frame, "Wrrong UserName or password, Retry!");
+        }
+
+    }
+
+  
+    
+}
