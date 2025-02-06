218c9970d16503d1e1ef1415dfc4ec2ddd3c9eae
vessahnjoya <vessahnjoyatchuentelouisnathan@gmail.com>
2025-02-07 00:56:12 +0100

    removed package declaration

--- LoginUI/SignUpListener.java
+++ LoginUI/SignUpListener.java
@@ -1,4 +1,4 @@
-package LoginUI;
+
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
--- LoginUI/SignUpManager.java
+++ LoginUI/SignUpManager.java
@@ -1,4 +1,4 @@
-package LoginUI;
+
 
 import java.io.FileReader;
 import java.io.FileWriter;
