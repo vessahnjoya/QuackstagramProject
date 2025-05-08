ec6cdb5c305f81be8e4ecbdb12986f2f1f69001f
Louis Nathan Vessah Njoya Tchuente <vessahnjoyatchuentelouisnathan@gmail.com>
2025-05-08 16:34:53 +0200

    added a test method

--- Database/DatabaseConnection.java
+++ Database/DatabaseConnection.java
@@ -2,17 +2,25 @@ package Database;
 
 import java.sql.*;
 
+/**
+ * This class hanles the database connection and contains credentials to login
+ * into the database and a test method to
+ * ensure the database is accessible
+ */
 public class DatabaseConnection {
 
     private static String dbUrl = "jdbc:mysql://localhost:3306/Quackstagram";
     private static String username = "Yurpi";
     private static String password = "senior_Rascar";
 
+    /**
+     * This method creates the database connection
+     */
     private void createDataBase() {
         try (Connection conn = DriverManager.getConnection(dbUrl, username, password)) {
-            System.out.println("done");
+
         } catch (Exception e) {
-            System.out.println("failed:" + e.getMessage());;
+            System.out.println("failed:" + e.getMessage());
         }
     }
 
@@ -20,4 +28,23 @@ public class DatabaseConnection {
         DatabaseConnection dbconnection = new DatabaseConnection();
         dbconnection.createDataBase();
     }
+
+    /**
+     * Test Method
+     */
+    // private void TestDbconnection(){
+    // try (Connection conn = DriverManager.getConnection(dbUrl, username,
+    // password)) {
+    // Statement stmt = conn.createStatement();
+    // ResultSet resultSet = stmt.executeQuery(
+    // "SELECT * FROM users"
+    // );
+    // while (resultSet.next()) {
+    // System.out.println(resultSet.getString("username"));
+    // System.out.println(resultSet.getString("user_password"));
+    // }
+    // } catch (Exception e) {
+    // System.out.println("failed:" + e.getMessage());
+    // }
+    // }
 }
