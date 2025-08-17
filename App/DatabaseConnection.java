6c7c941dd661ea25dbdc320cc796f9daa3cdc666
Louis Nathan Vessah Njoya Tchuente <vessahnjoyatchuentelouisnathan@gmail.com>
2025-08-17 10:42:49 +0200

    added mysql connection driver and fixedpassword

--- App/DatabaseConnection.java
+++ App/DatabaseConnection.java
@@ -7,7 +7,7 @@ import java.sql.SQLException;
 public class DatabaseConnection {
     private static final String URL = "jdbc:mysql://localhost:3306/Quackstagram";
     private static final String USERNAME = "njoya";
-    private static final String PASSWORD = "senior_Rascar";
+    private static final String PASSWORD = "senior_Rascar!";
 
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(URL, USERNAME, PASSWORD);
